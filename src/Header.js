ad6d90866554e2ca34fc94a3a518816f83296927
Matthew Chan <chanmj96@gmail.com>
2020-11-14 22:48:09 -0800

    Build site structure

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+
+function Header() {
+    return (
+        <div className="header">
+            <div><span>Icon</span></div>
+
+            <div className="navigation">
+                <span>Home</span>
+                <span>Projects</span>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
