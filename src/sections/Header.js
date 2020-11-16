1b35a9437892c0c08904ec0e6bd0617fe10dc8d8
Matthew Chan <chanmj96@gmail.com>
2020-11-15 22:16:22 -0800

    Create and include logo icon

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -1,8 +1,9 @@
+import logo from '../images/logo.svg';
 
 function Header() {
     return (
         <div className="header">
-            <div><span>Icon</span></div>
+            <div><span><img alt="logo" className="logo-icon" src={logo} /></span></div>
 
             <div className="navigation">
                 <span>Home</span>
